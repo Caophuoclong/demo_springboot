bbadcc5acb2ceaee92e81de2e21c2d814657bbbc
caophuoclong <caophuoclong.se@gmail.com>
2023-03-08 22:01:05 +0700

    first init for springboot project

---
+++ src/main/java/com/bpmn/bpmn_project/BpmnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bpmn.bpmn_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmnProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpmn/bpmn_project/BpmnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpmn.bpmn_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
